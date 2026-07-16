fc14610291f0706fe9b56bdaa49223477d26d27d
Sarika Kancharla <sarikareddy1804@gmail.com>
2026-07-16 23:55:59 +0530

    Configure app for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import gradio as gr
 from backend.services.image_service import get_place_image
 from backend.controller import generate_complete_trip
@@ -340,4 +341,7 @@ with gr.Blocks(
 ]
     )
 
-app.launch(share=True)
+app.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
